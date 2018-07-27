b7c3730d44ab3ed4e48220f01f823a6b6f4e7742
root <root@localhost.localdomain>
2018-07-27 11:30:43 -0400

    Added network_scanner.py file

---
+++ network_scanner.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP / IP range.")
+    options = parser.parse_args()
+    message = "\n\n[-] Please specify a target IP range. Use --help for more info.\n"
+    example = "\nExample: python network_scanner.py -t 192.168.1.0/24\n"
+    if not options.target:
+        parser.error(message + example)
+    return options
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+def print_results(results_list):
+    print("\nIP\t\t\tMAC Address\n--------------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_results(scan_result)
